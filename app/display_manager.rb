c1694607987cde0c6ca07754bfd6cc7474d67975
the-S-U-I-T <nikijsp@windowslive.com>
2025-07-04 08:50:09 +0300

    fix: add change to format_transaction_result function

--- app/display_manager.rb
+++ app/display_manager.rb
@@ -8,11 +8,11 @@ class DisplayManager
   end
 
   def format_product_list(products)
-    "Available Products:\n B2 - Soda Pop - 2.20"
+    "Available Products:\n #{products.map { |code, details| "#{code} - #{details[:name]}: #{details[:price]}" }.join("\n") }"
   end
 
   def format_transaction_result(product_name, change)
-    basic_result = "Dispensed #{product_name}"
+    basic_result = "Dispensed #{product_name} with change #{change}"
     last_transaction = @transaction_processor.last_transaction_details
 
     basic_result
